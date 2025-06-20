a0bcf8b5c7c980487eed7d70334e4aec5476bf5d
kevohm <kevinkbet@gmail.com>
2025-06-20 17:14:47 +0300

    fix: linkedin link

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -101,7 +101,7 @@ const About = () => {
                           <a
                             target="_blank "
                             className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
-                            href="https://www.linkedin.com/in/kevin-kipkemboi-7294181ba"
+                            href="https://www.linkedin.com/in/kevin-kipkemboi-kibet"
                           >
                             <svg
                               stroke="currentColor"
